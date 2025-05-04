05bf468f67c7401b959dc0c5a18a78a2c5105648
Suman Bera <sumanbera@Sumans-MacBook-Air.local>
2025-05-04 20:27:30 +0530

    update hrEmail

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,8 @@
-// next.config.js
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable ESLint errors during builds
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Disable TypeScript errors during builds
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  // Optionally silence Webpack warnings
-  webpack(config, { dev, isServer }) {
-    config.ignoreWarnings = [
-      () => true, // Ignores all warnings
-    ];
-
-    return config;
-  },
-
-  // Optionally, disable React strict mode (to suppress dev warnings)
-  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
-
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.module.rules.push({
-        test: /pdf-parse\/test\/data\/.*\.pdf$/,
-        use: 'ignore-loader'
-      });
-    }
-    return config;
-  }
-};
